82aca41ccc23736780fe5fc2856d4f54a3430848
Davide Greco <grecodavide.work@proton.me>
2024-02-25 20:56:40 +0100

    added exec plugin, updated gitignore

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String test = new String("Hello world!");
+        System.out.println(test);
     }
 }
