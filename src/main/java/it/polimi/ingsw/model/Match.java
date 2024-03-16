72418f9cdb27ee1b4c9d238a0dc6975d2f95e17e
Antonino Ciancimino <antonino.ciancimino@mail.polimi.it>
2024-03-16 12:54:37 +0100

    Add some comments to Match class source

--- src/main/java/it/polimi/ingsw/model/Match.java
+++ src/main/java/it/polimi/ingsw/model/Match.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.model;
 
-import com.sun.org.apache.xml.internal.security.Init;
 import com.sun.tools.javac.util.Pair;
 
 import java.util.ArrayList;
@@ -22,6 +21,14 @@ public class Match {
     private boolean finished = false;
     private Pair<Objective, Objective> currentProposedObjectives;
 
+    /**
+     * Constructor to be used to initialize main Match attributes and allocate the attribute players List.
+     * @param maxPlayers maximum number of players to be added to the match, chosen by the first player to join
+     * @param initialsDeck deck of initial cards
+     * @param resourcesDeck deck of resource cards
+     * @param goldsDeck deck of gold cards
+     * @param objectivesDeck deck of objectives
+     */
     public Match(int maxPlayers, GameDeck<InitialCard> initialsDeck, GameDeck<ResourceCard> resourcesDeck, GameDeck<GoldCard> goldsDeck, GameDeck<Objective> objectivesDeck) {
         this.maxPlayers = maxPlayers;
         this.initialsDeck = initialsDeck;
@@ -33,6 +40,11 @@ public class Match {
     }
 
     // Called by the controller
+    /**
+     * Method that adds a new player to the match; if the player is already in, throws an IllegalArgument exception.
+     * @param player player to be added to the match
+     * @throws IllegalArgumentException thrown if player already in the match
+     */
     public void addPlayer(Player player) throws IllegalArgumentException {
         if(!players.contains(player))
             players.add(player);
@@ -41,35 +53,63 @@ public class Match {
     }
 
     // Called by the controller
+    /**
+     *
+     * @param player
+     */
     public void removePlayer(Player player) {
         players.remove(player);
     }
 
     // Called by the controller
+    /**
+     *
+     * @return
+     */
     public boolean isFull() {
         return players.size() == maxPlayers;
     }
 
     // Called by the state
+    /**
+     *
+     */
     protected void doFinish() {
         finished = true;
     }
 
     // Called by the controller
+    /**
+     *
+     * @return
+     */
     public boolean isFinished() {
         return finished;
     }
 
     // Called by the controller
+    /**
+     *
+     * @return
+     */
     public Player getCurrentPlayer() {
         return currentPlayer;
     }
 
     // Called by the state
+
+    /**
+     *
+     * @param state
+     */
     protected void setState(MatchState state) {
         this.currentState = state;
     }
 
+    /**
+     *
+     * @return
+     */
     protected Pair<Objective, Objective> proposeSecretObjectives() {
         Objective obj1 = objectivesDeck.pop();
         Objective obj2 = objectivesDeck.pop();
@@ -78,14 +118,23 @@ public class Match {
     }
 
     // Called by the controller
+    /**
+     *
+     * @param objective
+     */
     protected void chooseSecretObjective(Objective objective) {
         // Put back the player's refused secret objective
         objectivesDeck.add(objective);
     }
 
+    /**
+     *
+     */
     protected void setupPlayers() {
-        // Randomize players
+        // Randomize players' turns
         Collections.shuffle(players);
+        
+        // Initialize currentPlayer
         currentPlayer = players.getFirst();
 
         // Set colors to players
@@ -93,51 +142,57 @@ public class Match {
             players.get(i).setColor(Color.values()[i]);
         }
     }
+
+    /**
+     *
+     */
     protected void setupDecks() {
-        // Shuffle decks
+        // Shuffle each deck
         initialsDeck.shuffle();
         resourcesDeck.shuffle();
         goldsDeck.shuffle();
         objectivesDeck.shuffle();
 
-        // Cards on the table
-        GoldCard goldCard1 = goldsDeck.pop();
-        GoldCard goldCard2 = goldsDeck.pop();
-
+        // Pop two resources to be placed on the common table
         ResourceCard resourceCard1 = resourcesDeck.pop();
         ResourceCard resourceCard2 = resourcesDeck.pop();
 
+        // Pop two golds to be placed on the common table
+        GoldCard goldCard1 = goldsDeck.pop();
+        GoldCard goldCard2 = goldsDeck.pop();
+
+        // Pop two golds to be placed on the common table
         Objective objective1 = objectivesDeck.pop();
         Objective objective2 = objectivesDeck.pop();
 
+        // Set popped cards in Match attributes
         visibleGolds = new Pair<>(goldCard1, goldCard2);
         visibleResources = new Pair<>(resourceCard1, resourceCard2);
         visibleObjectives = new Pair<>(objective1, objective2);
-
-        // Shuffle players??
-        // Set first user turn
-        currentPlayer = players.getFirst();
     }
 
+    /**
+     *
+     */
     protected void setupBoards() {
-        // Give cards to players
+        // Give starting cards to players
         for (Player player : players) {
+            // Pop a card from the resources deck and one from the golds deck
             GoldCard goldCard = goldsDeck.pop();
             ResourceCard resourceCard1 = resourcesDeck.pop();
             ResourceCard resourceCard2 = resourcesDeck.pop();
 
+            // Add each card to the player's hand
             player.getBoard().addHandCard(goldCard);
             player.getBoard().addHandCard(resourceCard1);
             player.getBoard().addHandCard(resourceCard2);
 
-            // Add initial card to the player's board
-            // By default, the initial card is provided on front side
+            // Place the initial card to the player's board
+            // By default, the initial card is placed on front side
             Pair<Integer, Integer> initialCoords = new Pair<>(0,0);
             InitialCard initial = initialsDeck.pop();
             player.getBoard().placeCard(initialCoords, initial, Side.FRONT);
 
         }
     }
-
-
 }
