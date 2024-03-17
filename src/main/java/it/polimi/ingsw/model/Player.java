d78be96b5bc35a05321d61553e4e5df70b0e4844
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2024-03-17 14:00:46 +0100

    Add some methods to Player class

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -9,17 +9,53 @@ public class Player {
     private Match match;
     private int points;
     private Board board;
-    private Map<Symbol, Integer> resources;
     private Color color;
     private Objective objective;
 
     public Player(String nickname, Match match) {
+        this.nickname = nickname;
+        this.match = match;
+         
+        //Initialize values
+        board = new Board();
+        points = 0;
+
     }
 
     public void playCard(Pair<Integer, Integer> coord, PlayableCard card, Side side) {
+        //TODO
     }
 
+    public void drawcard(DrawSource source) {
+        // TODO
+    }
+
+    public void chooseSecretObjective(Objective objective) {
+        // TODO
+    }
     public Board getBoard() {
         return board;
     }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    protected void setColor(Color color) {
+        this.color = color;
+    }
+
+    protected Objective getSecretObjective() {
+        return objective;
+    }
+
+    protected void addPoints(int points) {
+        this.points += points;
+    }
+
+
 }
