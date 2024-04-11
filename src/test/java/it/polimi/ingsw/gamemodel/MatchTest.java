81731829de3f0c6603dab9dfae272a31d4ddbd35
Antonino Ciancimino <antonino.ciancimino@mail.polimi.it>
2024-04-11 20:03:54 +0200

    Add tests to MatchTest
    Methods test added/finished:
     - drawCard
     - makeMove

--- src/test/java/it/polimi/ingsw/gamemodel/MatchTest.java
+++ src/test/java/it/polimi/ingsw/gamemodel/MatchTest.java
@@ -9,13 +9,9 @@ import java.util.Set;
 
 import javax.management.RuntimeErrorException;
 
-import it.polimi.ingsw.exceptions.CardException;
-import it.polimi.ingsw.exceptions.WrongChoiceException;
+import it.polimi.ingsw.exceptions.*;
 import it.polimi.ingsw.utils.Pair;
 import org.junit.Test;
-
-import it.polimi.ingsw.exceptions.InvalidResourceException;
-import it.polimi.ingsw.exceptions.WrongStateException;
 
 import static org.junit.Assert.*;
 
@@ -177,34 +173,45 @@ public class MatchTest {
     @Test
     public void removePlayer() {
         initializeAllEqualDecks();
+
         match = new Match(4, initialsDeck, resourcesDeck, goldsDeck, objectivesDeck);
+
         player1 = new Player("Oingo", match);
         player2 = new Player("Boingo", match);
         player3 = new Player("Jotaro", match);
         player4 = new Player("Polnareff", match);
 
-        // Test in waitstate
+        // Tests while Match is in WaitState: removePlayer can be called
         try {
             match.addPlayer(player1);
             match.addPlayer(player2);
+
+            // Verify that Match correctly removes a player
             match.removePlayer(player1);
             assertFalse("Player not removed", match.getPlayers().contains(player1));
+
+            // Match should now be full and switch to NextTurnState
             match.addPlayer(player1);
             match.addPlayer(player3);
             match.addPlayer(player4);
         } catch (Exception e) {
             fail("No exception must be thrown, exception thrown:" + e.getMessage());
         }
+
+        // Verify that Match is in the right state, that is NextTurnState
         assertTrue("Match not in correct state", match.getCurrentState() instanceof NextTurnState);
-        // Test outside of waitstate
+
+        // Match is not in NextTurnState
+        // Tests while Match is NOT in WaitState: removePlayer cannot be called
         try {
+            // Verify that Match throws an exception when the method is called while being in the wrong state
             match.removePlayer(player1);
-        } catch (Exception e) {
-            fail("No exception must be thrown, exception thrown:" + e.getMessage());
-        }
-        assertFalse("Player was not removed", match.getPlayers().contains(player1));
-        // TODO end match
-        //assertTrue("Match has not ended after player quit in game", match.isFinished());
+            // An exception is supposed to be thrown here
+            fail("match.removePlayer called in wrong state and an exception hasn't been thrown");
+        } catch (PlayerQuitException e) {}
+
+        // Verify that Match, after a player quit in the wrong state, is in FinalState
+        assertTrue("Match is not in FinalState even if a player quit in the wrong state", match.getCurrentState() instanceof FinalState);
     }
 
     @Test
@@ -367,7 +374,7 @@ public class MatchTest {
 
         // Try placing a card in the wrong coordinates
         try {
-            match.makeMove(new Pair<>(1, 4), match.getCurrentPlayer().getBoard().getCurrentHand().get(0), Side.FRONT);
+            match.makeMove(new Pair<>(1, 4), match.getCurrentPlayer().getBoard().getCurrentHand().get(1), Side.FRONT);
             fail("Should have thrown a WrongChoiceException");
         } catch (WrongChoiceException e) {
             // Good
@@ -378,34 +385,151 @@ public class MatchTest {
         // Try placing that is not in the player hand
         try {
             match.makeMove(new Pair<>(1, 1), generateRandomResourceCard(), Side.FRONT);
-            fail("Should have thrown CardException");
-        } catch (CardException e) {
+            fail("Should have thrown WrongChoiceException");
+        } catch (WrongChoiceException e) {
             // Good
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        // Try placing a card that the user does not enough resources to
+        PlayableCard card = match.getCurrentPlayer().getBoard().getCurrentHand().get(0);
+        try {
+            match.makeMove(new Pair<>(1, 1), card, Side.FRONT);
+            fail("Card placed even though there were not enough resources available");
+        } catch (Exception e) {
+            // Good
+        }
 
         // Try placing the card in the right position
-        PlayableCard card = match.getCurrentPlayer().getBoard().getCurrentHand().get(0);
+        card = match.getCurrentPlayer().getBoard().getCurrentHand().get(1);
+        // Note: now user has 2 Fungus
+
         try {
             match.makeMove(new Pair<>(1, 1), card, Side.BACK);
         } catch (Exception e) {
-            fail("Exception thrown when no exception expected");
+            fail("Exception thrown when no exception expected" + e.getMessage());
         }
 
         assertEquals("Card not placed", card, match.getCurrentPlayer().getBoard().getPlacedCards().get(new Pair<>(1,1)).getCard());
         assertEquals("Card not removed from hand", 2, match.getCurrentPlayer().getBoard().getCurrentHand().size());
-
         assertTrue("State not changed after makeMove", match.getCurrentState() instanceof AfterMoveState);
 
+        // Make the game go on
+        try {
+            match.getCurrentPlayer().drawCard(DrawSource.RESOURCES_DECK);
+            match.makeMove(new Pair<>(1,1), match.getCurrentPlayer().getBoard().getCurrentHand().get(1), Side.BACK);
+            match.getCurrentPlayer().drawCard(DrawSource.RESOURCES_DECK);
+        } catch (Exception e) {
+            fail(match.getCurrentState().getClass().getSimpleName());
+            throw new RuntimeException(e);
+        }
+        // Test placing a gold card with enough resources, should not fail
+        PlayableCard card2 = match.getCurrentPlayer().getBoard().getCurrentHand().get(0);
+        try {
+            Player curr = match.getCurrentPlayer();
+            int oldplayerpoints = curr.getPoints();
+            match.makeMove(new Pair<>(2,2), card2, Side.FRONT);
+            assertEquals(oldplayerpoints + card2.points, match.getCurrentPlayer().getPoints());
+        } catch (Exception e) {
+            fail("No exception should be thrown: " + e.getMessage());
+        }
     }
 
+    @Test
+    public void drawCard() {
+        initializeBlankStartedMatch(2, 2, 10, 9, 7);
+
+        // Try drawing card in the wrong state
+        try {
+            match.drawCard(DrawSource.FIRST_VISIBLE);
+            // An exception is supposed to be thrown here
+            fail("Should have thrown WrongStateException");
+        }
+        catch (WrongStateException e) {}
+        catch (Exception e) {
+            fail("Wrong exception launched: " + e.getMessage());
+        }
+        assertEquals(3, goldsDeck.getSize());
+        assertEquals(3, resourcesDeck.getSize());
+        // NOTE: at this point of the game
+        // resourcesDeck: 3 card goldsDeck: 3 cards
+        // Try drawing a card from FIRST_VISIBLE
+        // Draw from every source
+        DrawSource[] sources = DrawSource.values();
+        int j = 1;
+        for (int i = 0; i < 6; i++) {
+            try {
+                Player p = match.getCurrentPlayer();
+
+                p.playCard(new Pair<>(j, j), match.getCurrentPlayer().getBoard().getCurrentHand().get(1), Side.BACK);
+                // Increment indexes for the next player
+                if (match.getPlayers().getLast().equals(match.getCurrentPlayer())) j++;
+                PlayableCard card = match.drawCard(sources[i]);
+                p.getBoard().addHandCard(card);
+                assertNotNull(card);
+                // Check right replacement
+                if (i == 0) {
+                    assertTrue("Wrong state reached after draw", match.getCurrentState() instanceof NextTurnState);
+                    assertEquals(2, goldsDeck.getSize());
+                } else if (i == 1) {
+                    assertTrue("Wrong state reached after draw", match.getCurrentState() instanceof NextTurnState);
+                    assertEquals(2, resourcesDeck.getSize());
+                } else if (i <= 3) {
+                    assertTrue("Wrong state reached after draw", match.getCurrentState() instanceof NextTurnState);
+                    assertTrue("Wrong card placed", match.getVisiblePlayableCards().get(sources[i]) instanceof GoldCard);
+                } else {
+                    if (i == 4) {
+                        assertTrue("Wrong state reached after draw", match.getCurrentState() instanceof NextTurnState);
+                    } else {
+                        assertTrue("Wrong state reached after draw", match.getCurrentState() instanceof FinalState);
+                    }
+                    assertTrue("Wrong card placed", match.getVisiblePlayableCards().get(sources[i]) instanceof ResourceCard);
+                }
+            } catch (Exception e) {
+                fail("Should not have launched an exception: " + e.getMessage());
+            }
+        }
+        // Check the state
+        // Now all the decks should be empty and the match should be finished
+        assertEquals(0, goldsDeck.getSize());
+        assertEquals(0, resourcesDeck.getSize());
+        assertTrue(match.isFinished());
+
+        // Try drawing from an empty deck
+        initializeBlankStartedMatch(2, 2, 10, 6, 4);
+        j=1;
+        assertFalse(match.isFinished());
+        // Draw from the two empty decks
+        for (int i = 0; i < 2; i++) {
+            try {
+                match.getCurrentPlayer().playCard(new Pair<>(j, j), match.getCurrentPlayer().getBoard().getCurrentHand().get(1), Side.BACK);
+                match.drawCard(sources[i]);
+            } catch (WrongChoiceException e) {
+                // Go on with the state, correct exception launched
+                try {
+                    match.drawCard(sources[i + 2]);
+                } catch (Exception e2) {
+                    throw new RuntimeException(e2);
+                }
+            } catch (Exception e) {
+                fail("Wrong exception launched: " + e.getMessage() + " " + match.getCurrentState().getClass().getSimpleName());
+            }
+        }
+        assertTrue(match.isFinished());
+    }
+
+
+    // Private helper Methods
     private void initializeAllEqualDecks() {
         // Initialize Initial Cards deck
-        initialsDeck = createDeterministicInitialsDeck(4);
-        objectivesDeck = createDeterministicObjectivesDeck(10);
-        goldsDeck = createGoldsDeck(40);
-        resourcesDeck = createResourcesDeck(40);
+        initializeAllEqualDecks(4, 10, 40, 40);
+    }
+
+    private void initializeAllEqualDecks(int initialsNum, int objectsNum, int resourcesNum, int goldsNum) {
+        initialsDeck = createDeterministicInitialsDeck(initialsNum);
+        objectivesDeck = createDeterministicObjectivesDeck(objectsNum);
+        goldsDeck = createDeterministicGoldsDeck(goldsNum);
+        resourcesDeck = createDeterministicResourcesDeck(resourcesNum);
     }
 
     private static GameDeck<Objective> createDeterministicObjectivesDeck(int size) {
@@ -413,7 +537,7 @@ public class MatchTest {
         GameDeck<Objective> objectivesDeck = new GameDeck<>();
         Objective objective = null;
         HashMap<Symbol, Integer> resources = new HashMap<>();
-        resources.put(Symbol.FUNGUS, 2);
+        resources.put(Symbol.ANIMAL, 90);
         try {
             Requirement req = new QuantityRequirement(resources);
         } catch (Exception e) {
@@ -433,7 +557,7 @@ public class MatchTest {
         for (int i = 0; i < size; i++) {
             card = new InitialCard(
                     new CardFace(Symbol.FUNGUS, Symbol.ANIMAL, Symbol.PLANT, Symbol.INSECT, Collections.emptySet()),
-                    new CardFace(Symbol.EMPTY_CORNER, Symbol.EMPTY_CORNER, Symbol.EMPTY_CORNER, Symbol.EMPTY_CORNER,
+                    new CardFace(Symbol.FULL_CORNER, Symbol.FULL_CORNER, Symbol.FULL_CORNER, Symbol.FULL_CORNER,
                             Collections.emptySet()));
             initialsDeck.add(card);
         }
@@ -446,9 +570,10 @@ public class MatchTest {
         for (int i = 0; i < size; i++) {
             try {
                 card = new ResourceCard(
-                        new CardFace(Symbol.EMPTY_CORNER, Symbol.EMPTY_CORNER, Symbol.EMPTY_CORNER, Symbol.EMPTY_CORNER,
+                        new CardFace(Symbol.FULL_CORNER, Symbol.FULL_CORNER, Symbol.FULL_CORNER, Symbol.FULL_CORNER,
                                 Collections.emptySet()),
                         Symbol.FUNGUS, 0);
+                resourcesDeck.add(card);
             } catch (Exception e) {
                 throw new RuntimeException();
             }
@@ -460,12 +585,13 @@ public class MatchTest {
         GameDeck<GoldCard> goldsDeck = new GameDeck<>();
         GoldCard card = null;
         HashMap<Symbol, Integer> resources = new HashMap<>();
-        resources.put(Symbol.FUNGUS, 1);
+        resources.put(Symbol.FUNGUS, 2);
         for (int i = 0; i < size; i++) {
             try {
-                card = new GoldCard(new CardFace(Symbol.EMPTY_CORNER, Symbol.EMPTY_CORNER, Symbol.EMPTY_CORNER,
-                        Symbol.EMPTY_CORNER, Collections.emptySet()), Symbol.FUNGUS, Symbol.FEATHER, 3,
+                card = new GoldCard(new CardFace(Symbol.FULL_CORNER, Symbol.FULL_CORNER, Symbol.EMPTY_CORNER,
+                        Symbol.FULL_CORNER, Collections.emptySet()), Symbol.FUNGUS, Symbol.NO_MULT, 3,
                         new QuantityRequirement(resources));
+                goldsDeck.add(card);
             } catch (Exception e) {
                 throw new RuntimeErrorException(null);
             }
@@ -574,18 +700,17 @@ public class MatchTest {
         return null;
     }
 
-    private void initializeBlankMatch(int maxPlayers) {
+    private void initializeBlankMatch(int maxPlayers, int initialsNum, int objectivesNum, int resourcesNum, int goldsNum) {
         // Setup a basic game
         String names[] = {"Oingo", "Boingo", "Jotaro", "Polnareff"};
         Player players[] = new Player[4];
 
-        initializeAllEqualDecks();
+        initializeAllEqualDecks(initialsNum, objectivesNum, resourcesNum, goldsNum);
 
         match = new Match(maxPlayers, initialsDeck, resourcesDeck, goldsDeck, objectivesDeck);
 
         for (int i = 0; i < maxPlayers; i++) {
             players[i] = new Player(names[i], match);
-
             try {
                 match.addPlayer(players[i]);
             } catch (Exception e) {
@@ -594,9 +719,13 @@ public class MatchTest {
         }
     }
 
-    private void initializeBlankStartedMatch(int maxPlayers) {
+    protected void initializeBlankMatch(int maxPlayers) {
+        initializeBlankMatch(maxPlayers, 4, 10, 40, 40);
+    }
+
+    private void initializeBlankStartedMatch(int maxPlayers, int initialsNum, int objectivesNum, int resourcesNum, int goldsNum) {
         // Setup a basic game
-        initializeBlankMatch(maxPlayers);
+        initializeBlankMatch(maxPlayers, initialsNum, objectivesNum, resourcesNum, goldsNum);
 
         try {
             for (int i = 0; i < maxPlayers; i++) {
@@ -611,5 +740,9 @@ public class MatchTest {
         } catch (Exception e) {
                 throw new RuntimeException(e);
         }
+    }
+
+    private void initializeBlankStartedMatch(int maxPlayers) {
+        initializeBlankStartedMatch(maxPlayers, 4, 10, 40, 40);
     }
 }
