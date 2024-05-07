5f39d9323011cf7e580adf72f6e9ad26f09b4954
Gianluca Di Paola <gianluca.dipaola@mail.polimi.it>
2024-05-07 21:39:35 +0200

    Refactor, add various specific JsonExtractors

--- src/main/java/it/polimi/ingsw/utils/TUICardPrinter.java
+++ src/main/java/it/polimi/ingsw/utils/TUICardPrinter.java
@@ -1,7 +1,16 @@
 package it.polimi.ingsw.utils;
 
-import it.polimi.ingsw.gamemodel.InitialCard;
-import it.polimi.ingsw.gamemodel.Objective;
+import it.polimi.ingsw.gamemodel.*;
+
+import java.io.IOException;
+import com.google.gson.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class TUICardPrinter {
 
@@ -31,36 +40,61 @@ public class TUICardPrinter {
     /**
      * The method is called by the TUI View, in order to get what to print on the terminal.
      * @param cardID ID of the requested card
-     * @param isFacingUp is the card facing up? If not, it's facing down.
+     * @param filePath path of the json file to process
+     * @param isFacingUp is the card facing up? If not, it's facing down
+     * @param startingCoord x and y coordinates (determined from the terminal size and the board status) in which the card will be rendered
      * @return the string containing the series characters representing the side on which the card was played.
+     * @throws IOException if needed
      */
+    public String getPrintableCardAsStringWithCoord(int cardID, String filePath, Boolean isFacingUp, Pair<Integer, Integer> startingCoord) throws IOException {
 
-    public String getPrintableCardAsString(int cardID, Boolean isFacingUp){
+        int rows = startingCoord.first(),
+            cols = startingCoord.second();
+        StringBuffer printableCard = new StringBuffer("\"\"\"\n");
+
+        try {
+            if(filePath.contains("initial")) {
+                // recupero le informazioni della card
+                Map<String, List<Symbol>> map = initialJsonExtractor(filePath, cardID, isFacingUp);
+                // ...
+            }
+            else if (filePath.contains("gold")) {
+                goldJsonExtractor(filePath, cardID, isFacingUp);
+                // ...
+            }
+            else if (filePath.contains("resource")) {
+                resourceJsonExtractor(filePath, cardID, isFacingUp);
+                // ...
+            }
+            else if (filePath.contains("objective")) {
+                objectiveJsonExtractor(filePath, cardID, isFacingUp);
+                // ...
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
         return "TBA";
     }
 
-    /**
-     * The method is called by the TUI View, in order to get what to print on the terminal.
-     * @param initialCard initial card object
-     * @param isFacingUp is the card facing up? If not, it's facing down.
-     * @return the string containing the series characters representing the side on which the card was played.
-     */
 
-    public String getPrintableCardAsString(InitialCard initialCard, Boolean isFacingUp){
-
-        return "TBA";
+    private static void processCornersAndCenter(JsonObject sideObject, Map<String, List<Symbol>> elements) {
+        // TBA
     }
 
-    /**
-     * The method is called by the TUI View, in order to get what to print on the terminal.
-     * @param objectiveCard objective card object
-     * @return the string containing the series characters representing the side on which the card was played.
-     */
 
-    public String getPrintableCardAsString(Objective objectiveCard){
-
-        return "TBA";
+    private static void initialJsonExtractor(String filePath, int id, boolean isFrontWanted) throws IOException {
+        // TBA
     }
 
+    private static void objectiveJsonExtractor(String filePath, int id, boolean isFrontWanted) throws IOException {
+        // TBA
+    }
+    private static void goldJsonExtractor(String filePath, int id, boolean isFrontWanted) throws IOException {
+        // TBA
+    }
+    private static void resourceJsonExtractor(String filePath, int id, boolean isFrontWanted) throws IOException {
+        // TBA
+    }
 }
