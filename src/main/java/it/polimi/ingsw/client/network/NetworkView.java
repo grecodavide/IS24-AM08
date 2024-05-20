336c6f103b503a17c4aee5d72d9d130a9d0d0f73
Davide Greco <davide1.greco@mail.polimi.it>
2024-05-20 09:12:17 +0200

    Add error handling

--- src/main/java/it/polimi/ingsw/client/network/NetworkView.java
+++ src/main/java/it/polimi/ingsw/client/network/NetworkView.java
@@ -19,6 +19,8 @@ public abstract class NetworkView implements RemoteViewInterface {
     public NetworkView(GraphicalView graphicalViewInterface) {
         this.graphicalInterface = graphicalViewInterface;
     }
+
+    public abstract void showError(String cause);
     
     /**
      * Asks the server to send a list of {@link AvailableMatch}
